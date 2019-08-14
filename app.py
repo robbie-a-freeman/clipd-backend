3be814b27b4ceac6fb3116b7e580cba64e37468f
Robert Freeman <zarohian@yahoo.com>
2019-08-13 21:34:31 -0500

    Account for the Heroku config var

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     app.run()
 
 import psycopg2
-DATABASE_URL = os.environ['postgresql-convex-91631']
+DATABASE_URL = os.environ['DATABASE_URL'] # config var in Heroku
 try:
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     conn.close()
