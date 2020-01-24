6281343f4478fa632d774414ddec9773e6c62edf
Robbie Freeman <robbie.a.freeman@gmail.com>
2020-01-24 15:03:41 -0500

    updated BAT file to account for VM named clipd

--- runWebsiteDev.bat
+++ runWebsiteDev.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
-cmd.exe /K "cd C:\Users\Robbie\Documents\GitHub\clipd&&C:&&set FLASK_APP=app&&set ^FLASK_ENV=development&&flask run -p 8000"
+CALL activate clipd
+cmd.exe /K "cd C:\Users\zaroh\Documents\GitHub\clipd&&C:&&set FLASK_APP=app&&set ^FLASK_ENV=development&&flask run -p 8000"
